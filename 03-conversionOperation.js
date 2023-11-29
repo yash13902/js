c850ea6015f59d3b5b52831557885d3dc2399eba
Yash Jasani <yashjasani@Yashs-MacBook-Air.local>
2023-11-29 11:52:45 +0530

    math functions

--- 03-conversionOperation.js
+++ 03-conversionOperation.js
@@ -22,3 +22,10 @@ console.log(str);
 
 console.log(undefined === 0);    
 
+var arr = ["yash", "jasani"];
+console.log(typeof arr);
+
+let max = 20;
+let min = 10;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
