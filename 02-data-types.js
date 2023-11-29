0cca0316bb8e1cc844878a77ec6c1890303139b5
Yash Jasani <yashjasani@Yashs-MacBook-Air.local>
2023-11-29 11:28:21 +0530

    basics

---
+++ 02-data-types.js
@@ -0,0 +1,21 @@
+"use strict"; //forces to use the newer version of js
+
+// primitive data types
+// number => 2^53
+// bigInt
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => not defined
+// symbol => uniqueness
+
+
+// non-primitive data types
+// ojbects 
+// arrays 
+//functions 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
---
+++ 03-conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33";
+
+let s = "33abc";
+
+console.log(score);
+
+let num = Number(score);
+
+let n = Number(s);
+
+console.log(typeof num);
+
+console.log(n); // NaN => not a number
+console.log(typeof n); //number
+
+var str = String(0);
+
+console.log(str);
+
+
+// ******************* OPERATIONS *******************
+
+console.log(undefined === 0);    
+
